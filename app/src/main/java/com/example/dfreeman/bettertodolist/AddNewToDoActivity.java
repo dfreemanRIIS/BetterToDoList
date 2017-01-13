4902a1ba0d6dd4c261fc607a12a6ce002fb2aec0
dfreemanRIIS <dfreeman@riis.com>
2017-01-13 16:48:32 -0500

    Wire up date picker

--- app/src/main/java/com/example/dfreeman/bettertodolist/AddNewToDoActivity.java
+++ app/src/main/java/com/example/dfreeman/bettertodolist/AddNewToDoActivity.java
@@ -1,6 +1,7 @@
 package com.example.dfreeman.bettertodolist;
 
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -42,5 +43,10 @@ public class AddNewToDoActivity extends AppCompatActivity {
         //Go back to menu
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
