ce0d6b8d266943378f22c1252c9e9a6803c6ce05
dfreemanRIIS <dfreeman@riis.com>
2017-01-13 15:29:52 -0500

    Now sets task name

--- app/src/main/java/com/example/dfreeman/bettertodolist/AddNewToDoActivity.java
+++ app/src/main/java/com/example/dfreeman/bettertodolist/AddNewToDoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddNewToDoActivity extends AppCompatActivity {
 
@@ -11,9 +12,25 @@ public class AddNewToDoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_to_do);
+
+        //Will need to delete old to do eventually ***
+        //Edit changes will bring you here, delete old one ***
+        //New one will be added
     }
 
     public void onDoneCreating(View view) {
+        //Create new toDoItem
+        ToDoItem newToDo = new ToDoItem();
+
+        //Set Name
+        EditText newTaskName = (EditText)findViewById(R.id.enter_name);
+        String newTaskNameString = newTaskName.getText().toString();
+        newToDo.setName(newTaskNameString);
+
+        //Submit new toDoItem
+        MainActivity.itemList.add(newToDo);
+
+        //Go back to menu
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
