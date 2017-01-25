408efc97e158271a1b4f2a58088e8063819fcca6
dfreemanRIIS <dfreeman@riis.com>
2017-01-25 11:06:43 -0500

    Remove unused getters and setters, linter-6

--- app/src/main/java/com/example/dfreeman/bettertodolist/DonsCalendar.java
+++ app/src/main/java/com/example/dfreeman/bettertodolist/DonsCalendar.java
@@ -18,26 +18,6 @@ public class DonsCalendar {
         this.day = day;
     }
 
-    //Getters and Setters
-    public int getYear() {
-        return year;
-    }
-    public int getMonth() {
-        return month;
-    }
-    public int getDay() {
-        return day;
-    }
-    public void setYear(int year) {
-        this.year = year;
-    }
-    public void setMonth(int month) {
-        this.month = month;
-    }
-    public void setDay(int day) {
-        this.day = day;
-    }
-
     public String toString() {
         String monthString = "";
         if (month == 0) {
